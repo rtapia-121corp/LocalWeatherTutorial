91ff470490a22db643dfb21b3a9ac807742b62f2
Ricardo Tapia <rtapia@121corp.com>
2023-07-28 17:40:09 -0600

    se comienza a crear formulario de envio de mails

--- src/components/EmailForm.vue
+++ src/components/EmailForm.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="pt-4 m-8 relative">
-        <form action="" method="post" class="from-black border-b">
+        <form @submit.prevent="sendEmail" class="from-black">
             <div class="space-y-12  text-white">
                 <div class="border-b border-gray-900/10 pb-12">
                     <h2 class="text-base font-semibold leading-7">Profile</h2>
-                    <p class="mt-1 text-sm leading-6">This information will be displayed publicly so be careful what you share.</p>
+                    <p class="mt-1 text-sm leading-6">This information will be displayed publicly so be careful what you
+                        share.</p>
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="sm:col-span-4">
                             <label for="email" class="block text-sm font-medium leading-6">Email address</label>
@@ -27,12 +28,29 @@
                     </div>
                 </div>
             </div>
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <button type="submit"
+                    class="rounded-md bg-weather-secondary px-8 py-3 text-sm font-semibold text-white shadow-sm hover:bg-weather-secondary 
+                     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-weather-secondary ">Send</button>
+            </div>
         </form>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import axios from "axios";
 
+const email = ref("");
+const promt = ref("");
+
+const sendEmail = () => {
+  queryTimeout.value = setTimeout(async () => {
+    if(searchQuery.value !== ''){
+      const result = await axios.post();
+    }
+  },300);
+}
 </script>
 
 <style lang="scss" scoped></style>
